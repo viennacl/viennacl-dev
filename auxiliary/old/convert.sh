0ed4f0f743d6ab4594fb307ece7e26660d45ab8b
Karl Rupp <rupp@iue.tuwien.ac.at>
2012-10-30 13:23:33 -0500

    Purged old/unused kernels in auxiliary folder

--- auxiliary/old/convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-g++ generate-blas3-solve-align1.cpp -o generate-blas3-solve-align1
-g++ generate-blas3-prod-align1.cpp -o generate-blas3-prod-align1
-
-./generate-blas3.sh
-
-g++ converter.cpp -o converter -lboost_filesystem-mt
-./converter
