b44fe90376aa1ccf3756f12758e26e88e52bd1d4
Philippe Tillet <phil.tillet@gmail.com>
2013-12-19 05:58:24 +0800

    BLAS Dispatcher : Cleaned the internals

--- viennacl/backend/blas.hpp
+++ viennacl/backend/blas.hpp
@@ -58,81 +58,85 @@ namespace viennacl
           TransposeType negtrans;
       };
 
-
-#ifdef VIENNACL_WITH_CBLAS
       template<class T>
-      struct cblas;
+      struct ram_handle {
+          typedef T * ptr_type;
+          typedef T const * const_ptr_type;
 
-      template<>
-      struct cblas<float>{
+          static ptr_type get(viennacl::backend::mem_handle & h){ return reinterpret_cast<ptr_type>(h.ram_handle().get()); }
+          static const_ptr_type get(viennacl::backend::mem_handle const & h){ return reinterpret_cast<const_ptr_type>(h.ram_handle().get()); }
+      };
+
+      template<class T, class OrderType, OrderType ColMajorVal, class TransposeType,
+               void (*_gemm)(const OrderType, const TransposeType, const TransposeType, const int, const int,
+                                   const int, const T, T const *, const int, T const *, const int, const T, T*, const int)>
+      class gemm_wrap{
+        public:
+          static void execute(const TransposeType transA, const TransposeType transB, const int M , const int N, const int K,
+                            const T alpha, T const * A, const int offa, const int lda,
+                           T const * B, const int offb, const int ldb,
+                           const T beta, T* C, const int offc, const int ldc)
+          {
+            _gemm(ColMajorVal, transA, transB, M, N, K, alpha, A+offa, lda, B+offb, ldb, beta, C+offc, ldc);
+          }
+      };
+
+#ifdef VIENNACL_WITH_CUDA
+      template<class T>
+      struct cuda_handle {
+          typedef T * ptr_type;
+          typedef T const * const_ptr_type;
+
+          static ptr_type get(viennacl::backend::mem_handle & h){  return reinterpret_cast<ptr_type>(h.cuda_handle().get()); }
+          static const_ptr_type get(viennacl::backend::mem_handle const & h){ return reinterpret_cast<const_ptr_type>(h.cuda_handle().get()); }
+      };
+
+#endif
+
+
+      template<typename ScalarType, typename HandleType, typename TransposeType, TransposeType Trans, TransposeType NoTrans, class GemmWrap>
+      struct blas_impl{
         private:
-          typedef matrix_blas<CBLAS_TRANSPOSE> wrapper;
+          typedef matrix_blas<TransposeType> wrapper;
         public:
           static bool gemm(bool C_row_major, bool A_row_major, bool B_row_major
                            ,bool is_A_trans, bool is_B_trans
-                           ,const vcl_size_t M, const vcl_size_t N, const vcl_size_t K, const float alpha
+                           ,const vcl_size_t M, const vcl_size_t N, const vcl_size_t K, const ScalarType alpha
                            ,viennacl::backend::mem_handle const hA, const vcl_size_t A_internal_size1, const vcl_size_t A_internal_size2
                            ,const vcl_size_t A_start1, const vcl_size_t A_start2, const vcl_size_t A_inc1, const vcl_size_t A_inc2
                            ,viennacl::backend::mem_handle const hB, const vcl_size_t B_internal_size1, const vcl_size_t B_internal_size2
                            ,const vcl_size_t B_start1, const vcl_size_t B_start2, const vcl_size_t B_inc1, const vcl_size_t B_inc2
-                           ,const float beta, viennacl::backend::mem_handle  hC, const vcl_size_t C_internal_size1, const vcl_size_t C_internal_size2
+                           ,const ScalarType beta, viennacl::backend::mem_handle  hC, const vcl_size_t C_internal_size1, const vcl_size_t C_internal_size2
                            ,const vcl_size_t C_start1, const vcl_size_t C_start2, const vcl_size_t C_inc1, const vcl_size_t C_inc2)
           {
             if(A_inc1!=1 || A_inc2!=1 || B_inc1!=1 || B_inc2!=1 || C_inc1!=1 || C_inc2!=1)
               return false;
 
-            float const * pA = reinterpret_cast<float*>(hA.ram_handle().get());
-            float const * pB = reinterpret_cast<float*>(hB.ram_handle().get());
-            float * pC = reinterpret_cast<float*>(hC.ram_handle().get());
+            typename HandleType::const_ptr_type pA = HandleType::get(hA);
+            typename HandleType::const_ptr_type pB = HandleType::get(hB);
+            typename HandleType::ptr_type pC = HandleType::get(hC);
 
-            wrapper A(CblasTrans, CblasNoTrans, A_row_major,is_A_trans,A_internal_size1, A_internal_size2, A_start1, A_start2, A_inc1, A_inc2);
-            wrapper B(CblasTrans, CblasNoTrans, B_row_major,is_B_trans,B_internal_size1, B_internal_size2, B_start1, B_start2, B_inc1, B_inc2);
-            wrapper C(CblasTrans, CblasNoTrans, C_row_major,false,C_internal_size1, C_internal_size2, C_start1, C_start2, C_inc1, C_inc2);
+            wrapper A(Trans, NoTrans, A_row_major,is_A_trans,A_internal_size1, A_internal_size2, A_start1, A_start2, A_inc1, A_inc2);
+            wrapper B(Trans, NoTrans, B_row_major,is_B_trans,B_internal_size1, B_internal_size2, B_start1, B_start2, B_inc1, B_inc2);
+            wrapper C(Trans, NoTrans, C_row_major,false,C_internal_size1, C_internal_size2, C_start1, C_start2, C_inc1, C_inc2);
 
             if(C_row_major)
-              cblas_sgemm(CblasColMajor,B.trans, A.trans, N, M, K, alpha, pB+B.off, B.ld, pA+A.off, A.ld, beta, pC+C.off, C.ld);
+              GemmWrap::execute(B.trans, A.trans, N, M, K, alpha, pB, B.off, B.ld, pA, A.off, A.ld, beta, pC, C.off, C.ld);
             else
-              cblas_sgemm(CblasColMajor,A.negtrans, B.negtrans, M, N, K, alpha, pA+A.off, A.ld, pB+B.off, B.ld, beta, pC+C.off, C.ld);
+              GemmWrap::execute(A.negtrans, B.negtrans, M, N, K, alpha, pA, A.off, A.ld, pB, B.off, B.ld, beta, pC, C.off, C.ld);
 
             return true;
           }
       };
 
+#ifdef VIENNACL_WITH_CBLAS
+      template<class T>  struct cblas;
 
-      template<>
-      struct cblas<double>{
-        private:
-          typedef matrix_blas<CBLAS_TRANSPOSE> wrapper;
-        public:
-          static bool gemm(bool C_row_major, bool A_row_major, bool B_row_major
-                           ,bool is_A_trans, bool is_B_trans
-                           ,const vcl_size_t M, const vcl_size_t N, const vcl_size_t K, const double alpha
-                           ,double const *hA, const vcl_size_t A_internal_size1, const vcl_size_t A_internal_size2
-                           ,const vcl_size_t A_start1, const vcl_size_t A_start2, const vcl_size_t A_inc1, const vcl_size_t A_inc2
-                           ,double const *hB, const vcl_size_t B_internal_size1, const vcl_size_t B_internal_size2
-                           ,const vcl_size_t B_start1, const vcl_size_t B_start2, const vcl_size_t B_inc1, const vcl_size_t B_inc2
-                           ,const double beta, double *hC, const vcl_size_t C_internal_size1, const vcl_size_t C_internal_size2
-                           ,const vcl_size_t C_start1, const vcl_size_t C_start2, const vcl_size_t C_inc1, const vcl_size_t C_inc2)
-          {
-            if(A_inc1!=1 || A_inc2!=1 || B_inc1!=1 || B_inc2!=1 || C_inc1!=1 || C_inc2!=1)
-              return false;
+      template<> struct cblas<float> : public blas_impl<float, ram_handle<float>, CBLAS_TRANSPOSE, CblasTrans, CblasNoTrans
+                                                    , gemm_wrap<float, CBLAS_ORDER, CblasColMajor, CBLAS_TRANSPOSE, cblas_sgemm> >{ };
 
-            wrapper A(CblasTrans, CblasNoTrans, A_row_major,is_A_trans,A_internal_size1, A_internal_size2, A_start1, A_start2, A_inc1, A_inc2);
-            wrapper B(CblasTrans, CblasNoTrans, B_row_major,is_B_trans,B_internal_size1, B_internal_size2, B_start1, B_start2, B_inc1, B_inc2);
-            wrapper C(CblasTrans, CblasNoTrans, C_row_major,false,C_internal_size1, C_internal_size2, C_start1, C_start2, C_inc1, C_inc2);
-
-            double const * pA = reinterpret_cast<double*>(hA.ram_handle().get());
-            double const * pB = reinterpret_cast<double*>(hB.ram_handle().get());
-            double * pC = reinterpret_cast<double*>(hC.ram_handle().get());
-
-            if(C_row_major)
-              cblas_dgemm(CblasColMajor,B.trans, A.trans, N, M, K, alpha, pB+B.off, B.ld, pA+A.off, A.ld, beta, pC+C.off, C.ld);
-            else
-              cblas_dgemm(CblasColMajor,A.negtrans, B.negtrans, M, N, K, alpha, pA+A.off, A.ld, pB+B.off, B.ld, beta, pC+C.off, C.ld);
-
-            return true;
-          }
-      };
+      template<> struct cblas<double> : public blas_impl<double, ram_handle<double>, CBLAS_TRANSPOSE, CblasTrans, CblasNoTrans
+                                                    , gemm_wrap<double, CBLAS_ORDER, CblasColMajor, CBLAS_TRANSPOSE, cblas_dgemm> >{ };
 #endif
 
 
@@ -227,18 +231,6 @@ namespace viennacl
                              ,const T /*beta*/, viennacl::backend::mem_handle /*C*/, const vcl_size_t /*C_internal_size1*/, const vcl_size_t /*C_internal_size2*/
                              ,const vcl_size_t /*C_start1*/, const vcl_size_t /*C_start2*/, const vcl_size_t /*C_inc1*/, const vcl_size_t /*C_inc2*/);
     };
-
-#define HAS_MEM_FUNC(func, name)                                        \
-  template<typename T, typename Sign>                                 \
-  struct name {                                                       \
-  typedef char yes[1];                                            \
-  typedef char no [2];                                            \
-  template <typename U, U> struct type_check;                     \
-  template <typename _1> static yes &chk(type_check<Sign, &_1::gemm> *); \
-  template <typename   > static no  &chk(...);                    \
-  static bool const value = sizeof(chk<T>(0)) == sizeof(yes);     \
-  }
-
 
 
     namespace detail{
