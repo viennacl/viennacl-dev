d7eccaf0e8dedfcc847302a7b6f3527b709bf188
Philippe Tillet <phil.tillet@gmail.com>
2013-12-19 13:45:44 +0800

    BLAS Linking : Added missing file

--- viennacl/backend/blas.hpp
+++ viennacl/backend/blas.hpp
@@ -24,12 +24,13 @@
 
 #include <cassert>
 
+#include "viennacl/backend/blas/gemm.hpp"
+
 #include "viennacl/backend/mem_handle.hpp"
 
 #ifdef VIENNACL_WITH_CBLAS
 #include "cblas.h"
 #endif
-
 
 #ifdef VIENNACL_WITH_CUBLAS
 #include "cublas.h"
@@ -78,40 +79,6 @@ namespace viennacl
       };
 
 #endif
-
-      namespace wrap{
-
-        template<class T, class OrderType, OrderType ColMajorVal, class TransposeType,
-                 void (*_gemm)(const OrderType, const TransposeType, const TransposeType, const int, const int,
-                                     const int, const T, T const *, const int, T const *, const int, const T, T*, const int)>
-        class gemm_cblas{
-          public:
-            static void execute(const TransposeType transA, const TransposeType transB, const int M , const int N, const int K,
-                              const T alpha, T const * A, const int offa, const int lda,
-                             T const * B, const int offb, const int ldb,
-                             const T beta, T* C, const int offc, const int ldc)
-            {
-              _gemm(ColMajorVal, transA, transB, M, N, K, alpha, A+offa, lda, B+offb, ldb, beta, C+offc, ldc);
-            }
-        };
-
-        template<class T, class TransposeType,
-                 void (*_gemm)(const TransposeType, const TransposeType, const int, const int,
-                                     const int, const T, T const *, const int, T const *, const int, const T, T*, const int)>
-        class gemm_blas{
-          public:
-            static void execute(const TransposeType transA, const TransposeType transB, const int M , const int N, const int K,
-                              const T alpha, T const * A, const int offa, const int lda,
-                             T const * B, const int offb, const int ldb,
-                             const T beta, T* C, const int offc, const int ldc)
-            {
-              _gemm(transA, transB, M, N, K, alpha, A+offa, lda, B+offb, ldb, beta, C+offc, ldc);
-            }
-        };
-
-      }
-
-
 
       template<typename ScalarType, typename HandleType, typename TransposeType, TransposeType Trans, TransposeType NoTrans, class GemmWrap>
       struct blas_impl{
@@ -171,14 +138,6 @@ namespace viennacl
 
 
 
-    namespace detail{
-      template<typename U, typename Ret>
-      struct init_gemm { static const Ret value() { return NULL; } };
-      template<template<class> class U, typename Ret>
-      struct init_gemm< U<float>, Ret> {  static const Ret value() { return &U<float>::gemm; } };
-      template<template<class> class U, typename Ret>
-      struct init_gemm< U<double>, Ret> {  static const Ret value() { return &U<double>::gemm; } };
-    }
 
     template<class T>
     class blas{
@@ -205,7 +164,6 @@ namespace viennacl
           gemm_ = detail::init_gemm< viennacl::backend::cublas<value_type>, gemm_type >::value();
 #endif
         }
-
 
         gemm_type gemm() const { return gemm_; }
         void gemm(gemm_type fptr) { gemm_ = fptr; }
