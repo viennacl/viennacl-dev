1fb37fe041621657c52625f74397ec0109c624bc
Karl Rupp <rupp@iue.tuwien.ac.at>
2014-01-20 22:03:00 +0100

    Updated copyright timespan to 2010-2014.

--- viennacl/generator/mapped_objects.hpp
+++ viennacl/generator/mapped_objects.hpp
@@ -2,7 +2,7 @@
 #define VIENNACL_GENERATOR_MAPPED_TYPE_HPP
 
 /* =========================================================================
-   Copyright (c) 2010-2013, Institute for Microelectronics,
+   Copyright (c) 2010-2014, Institute for Microelectronics,
                             Institute for Analysis and Scientific Computing,
                             TU Wien.
    Portions of this software are copyright by UChicago Argonne, LLC.
