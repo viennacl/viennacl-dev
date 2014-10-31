viennacl/viennacl-dev

commit a89695e58077a75bfe748f8b5b85b62092698061

Vector: Added support for resizing a vector<T> object with copy if it has zero size.
The following is now legitimate:
  std::vector<T> x(N);
  viennacl::vector<T> vcl_x;
  viennacl::copy(x, vcl_x);
and resizes the ViennaCL vector accordingly (in the default context).
A similar support for the iterator interface is not possible without substantial refactoring.

viennacl/vector.hpp
@@ -1471,14 +1471,27 @@ void copy(CPU_ITERATOR const & cpu_begin,
 
 // for things like copy(std_vec.begin(), std_vec.end(), vcl_vec.begin() + 1);
 
-/** @brief Transfer from a cpu vector to a gpu vector. Convenience wrapper for viennacl::linalg::copy(cpu_vec.begin(), cpu_vec.end(), gpu_vec.begin());
+/** @brief Transfer from a host vector object to a ViennaCL vector proxy. Requires the vector proxy to have the necessary size. Convenience wrapper for viennacl::linalg::copy(cpu_vec.begin(), cpu_vec.end(), gpu_vec.begin());
 *
 * @param cpu_vec    A cpu vector. Type requirements: Iterator can be obtained via member function .begin() and .end()
 * @param gpu_vec    The gpu vector.
 */
-template<typename CPUVECTOR, typename T>
-void copy(const CPUVECTOR & cpu_vec, vector_base<T> & gpu_vec)
+template<typename HostVectorT, typename T>
+void copy(HostVectorT const & cpu_vec, vector_base<T> & gpu_vec)
 {
+  viennacl::copy(cpu_vec.begin(), cpu_vec.end(), gpu_vec.begin());
+}
+
+/** @brief Transfer from a host vector object to a ViennaCL vector. Resizes the ViennaCL vector if it has zero size. Convenience wrapper for viennacl::linalg::copy(cpu_vec.begin(), cpu_vec.end(), gpu_vec.begin());
+*
+* @param cpu_vec    A host vector. Type requirements: Iterator can be obtained via member function .begin() and .end()
+* @param gpu_vec    The gpu (ViennaCL) vector.
+*/
+template<typename HostVectorT, typename T>
+void copy(HostVectorT const & cpu_vec, vector<T> & gpu_vec)
+{
+  if (gpu_vec.size() == 0)
+    gpu_vec.resize(static_cast<vcl_size_t>(cpu_vec.end() - cpu_vec.begin()));
   viennacl::copy(cpu_vec.begin(), cpu_vec.end(), gpu_vec.begin());
 }
 

commit 80192cf8e2ad8a1db2b3e7557029bcb93fc72c6f

Clang 3.0: Fixed compilation problems with new copy() overload for vector.
Issue introduced with a89695e58077a75bfe748f8b5b85b62092698061

viennacl/linalg/toeplitz_matrix_operations.hpp
@@ -61,11 +61,11 @@ namespace viennacl
 
 
 
-      copy(vec, tmp);
+      viennacl::copy(vec.begin(), vec.end(), tmp.begin());
       viennacl::linalg::real_to_complex(tmp, tmp2, vec.size() * 2);
       viennacl::linalg::convolve(tep, tmp2, tmp);
       viennacl::linalg::complex_to_real(tmp, tmp2, vec.size() * 2);
-      copy(tmp2.begin(), tmp2.begin() + static_cast<vcl_ptrdiff_t>(vec.size()), result.begin());
+      viennacl::copy(tmp2.begin(), tmp2.begin() + static_cast<vcl_ptrdiff_t>(vec.size()), result.begin());
     }
 
   } //namespace linalg

viennacl/vector.hpp
@@ -1487,8 +1487,8 @@ void copy(HostVectorT const & cpu_vec, vector_base<T> & gpu_vec)
 * @param cpu_vec    A host vector. Type requirements: Iterator can be obtained via member function .begin() and .end()
 * @param gpu_vec    The gpu (ViennaCL) vector.
 */
-template<typename HostVectorT, typename T>
-void copy(HostVectorT const & cpu_vec, vector<T> & gpu_vec)
+template<typename HostVectorT, typename T, unsigned int AlignmentV>
+void copy(HostVectorT const & cpu_vec, vector<T, AlignmentV> & gpu_vec)
 {
   if (gpu_vec.size() == 0)
     gpu_vec.resize(static_cast<vcl_size_t>(cpu_vec.end() - cpu_vec.begin()));