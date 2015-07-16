d2ef9b25e687d7d8196ae4ee027dc901e31b2c84
Karl Rupp <rupp@iue.tuwien.ac.at>
2015-07-16 16:22:29 +0200

    MIC: Enhanced kernel parameters for BLAS levels 1, 2, 3.
    Parameters not obtained from a full-fledged optimizer run,
    but from careful manual tweaking. Obtained memory bandwidths
    for BLAS levels 1 and 2 are about 70 GB/sec, which is okay given
    that vector operations on the Xeon Phi are slow with OpenCL.
    BLAS level 3 improves very mildly, peaks at about 40 GFLOP/sec.
    
    Given that OpenCL for Xeon Phi (KNC) has limited use and that
    everybody is eager for KNL, further tuning efforts are suspended.
    
    Resolves #26.

--- viennacl/device_specific/builtin_database/devices/accelerator/fallback.hpp
+++ viennacl/device_specific/builtin_database/devices/accelerator/fallback.hpp
@@ -19,93 +19,94 @@ namespace fallback{
 
 inline void add_4B(database_type<vector_axpy_template::parameters_type> & db)
 {
-  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", vector_axpy_template::parameters_type(1,128,128,FETCH_FROM_GLOBAL_CONTIGUOUS));
+  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", vector_axpy_template::parameters_type(1,32*128,1920,FETCH_FROM_GLOBAL_CONTIGUOUS));
 }
 
 inline void add_4B(database_type<reduction_template::parameters_type> & db)
 {
-  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", reduction_template::parameters_type(1,128,128,FETCH_FROM_GLOBAL_CONTIGUOUS));
+  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", reduction_template::parameters_type(1,32*64,32*64,FETCH_FROM_GLOBAL_CONTIGUOUS));
 }
 
 inline void add_4B(database_type<matrix_axpy_template::parameters_type> & db)
 {
-  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_axpy_template::parameters_type(1,8,8,8,8,FETCH_FROM_GLOBAL_CONTIGUOUS));
+  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_axpy_template::parameters_type(1,32,32,32,32,FETCH_FROM_GLOBAL_CONTIGUOUS));
 }
 
 inline void add_4B(database_type<row_wise_reduction_template::parameters_type> & db, char_to_type<'N'>)
 {
-  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", row_wise_reduction_template::parameters_type(1,1,128,128,FETCH_FROM_GLOBAL_CONTIGUOUS));
+  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", row_wise_reduction_template::parameters_type(1,1,32*64,32*64,FETCH_FROM_GLOBAL_CONTIGUOUS));
 }
 
 inline void add_4B(database_type<row_wise_reduction_template::parameters_type> & db, char_to_type<'T'>)
 {
-  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", row_wise_reduction_template::parameters_type(1,1,128,128,FETCH_FROM_GLOBAL_CONTIGUOUS));
+  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", row_wise_reduction_template::parameters_type(1,1,32*64,32*64,FETCH_FROM_GLOBAL_CONTIGUOUS));
 }
 
 inline void add_4B(database_type<matrix_product_template::parameters_type> & db, char_to_type<'N'>, char_to_type<'N'>)
 {
-  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,8,8,8,4,4,4,FETCH_FROM_LOCAL,FETCH_FROM_LOCAL,8,8));
+  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,16,32,16,1,1,1,FETCH_FROM_GLOBAL_CONTIGUOUS,FETCH_FROM_GLOBAL_CONTIGUOUS,0,0));
 }
 
 inline void add_4B(database_type<matrix_product_template::parameters_type> & db, char_to_type<'T'>, char_to_type<'N'>)
 {
-  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,8,8,8,4,4,4,FETCH_FROM_LOCAL,FETCH_FROM_LOCAL,8,8));
+  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,16,32,16,1,1,1,FETCH_FROM_GLOBAL_CONTIGUOUS,FETCH_FROM_GLOBAL_CONTIGUOUS,0,0));
+
 }
 
 inline void add_4B(database_type<matrix_product_template::parameters_type> & db, char_to_type<'N'>, char_to_type<'T'>)
 {
-  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,8,8,8,4,4,4,FETCH_FROM_LOCAL,FETCH_FROM_LOCAL,8,8));
+  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,16,32,16,1,1,1,FETCH_FROM_GLOBAL_CONTIGUOUS,FETCH_FROM_GLOBAL_CONTIGUOUS,0,0));
 }
 
 inline void add_4B(database_type<matrix_product_template::parameters_type> & db, char_to_type<'T'>, char_to_type<'T'>)
 {
-  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,8,8,8,4,4,4,FETCH_FROM_LOCAL,FETCH_FROM_LOCAL,8,8));
+  db.add_4B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,16,32,16,1,1,1,FETCH_FROM_GLOBAL_CONTIGUOUS,FETCH_FROM_GLOBAL_CONTIGUOUS,0,0));
 }
 
 
 inline void add_8B(database_type<vector_axpy_template::parameters_type> & db)
 {
-  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", vector_axpy_template::parameters_type(1,128,128,FETCH_FROM_GLOBAL_CONTIGUOUS));
+  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", vector_axpy_template::parameters_type(1,32*128,32*128,FETCH_FROM_GLOBAL_CONTIGUOUS));
 }
 
 inline void add_8B(database_type<reduction_template::parameters_type> & db)
 {
-  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", reduction_template::parameters_type(1,128,128,FETCH_FROM_GLOBAL_CONTIGUOUS));
+  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", reduction_template::parameters_type(1,32*128,32*128,FETCH_FROM_GLOBAL_CONTIGUOUS));
 }
 
 inline void add_8B(database_type<matrix_axpy_template::parameters_type> & db)
 {
-  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_axpy_template::parameters_type(1,8,8,8,8,FETCH_FROM_GLOBAL_CONTIGUOUS));
+  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_axpy_template::parameters_type(1,32,32,32,32,FETCH_FROM_GLOBAL_CONTIGUOUS));
 }
 
 inline void add_8B(database_type<row_wise_reduction_template::parameters_type> & db, char_to_type<'N'>)
 {
-  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", row_wise_reduction_template::parameters_type(1,1,128,128,FETCH_FROM_GLOBAL_CONTIGUOUS));
+  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", row_wise_reduction_template::parameters_type(1,1,32*64,32*64,FETCH_FROM_GLOBAL_CONTIGUOUS));
 }
 
 inline void add_8B(database_type<row_wise_reduction_template::parameters_type> & db, char_to_type<'T'>)
 {
-  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", row_wise_reduction_template::parameters_type(1,1,128,128,FETCH_FROM_GLOBAL_CONTIGUOUS));
+  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", row_wise_reduction_template::parameters_type(1,1,32*64,32*64,FETCH_FROM_GLOBAL_CONTIGUOUS));
 }
 
 inline void add_8B(database_type<matrix_product_template::parameters_type> & db, char_to_type<'N'>, char_to_type<'N'>)
 {
-  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,8,8,8,4,4,4,FETCH_FROM_LOCAL,FETCH_FROM_LOCAL,8,8));
+ db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,16,32,16,1,1,1,FETCH_FROM_GLOBAL_CONTIGUOUS,FETCH_FROM_GLOBAL_CONTIGUOUS,0,0));
 }
 
 inline void add_8B(database_type<matrix_product_template::parameters_type> & db, char_to_type<'T'>, char_to_type<'N'>)
 {
-  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,8,8,8,4,4,4,FETCH_FROM_LOCAL,FETCH_FROM_LOCAL,8,8));
+  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,16,32,16,1,1,1,FETCH_FROM_GLOBAL_CONTIGUOUS,FETCH_FROM_GLOBAL_CONTIGUOUS,0,0));
 }
 
 inline void add_8B(database_type<matrix_product_template::parameters_type> & db, char_to_type<'N'>, char_to_type<'T'>)
 {
-  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,8,8,8,4,4,4,FETCH_FROM_LOCAL,FETCH_FROM_LOCAL,8,8));
+  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,16,32,16,1,1,1,FETCH_FROM_GLOBAL_CONTIGUOUS,FETCH_FROM_GLOBAL_CONTIGUOUS,0,0));
 }
 
 inline void add_8B(database_type<matrix_product_template::parameters_type> & db, char_to_type<'T'>, char_to_type<'T'>)
 {
-  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,8,8,8,4,4,4,FETCH_FROM_LOCAL,FETCH_FROM_LOCAL,8,8));
+  db.add_8B(unknown_id, CL_DEVICE_TYPE_ACCELERATOR, unknown, "", matrix_product_template::parameters_type(1,16,32,16,1,1,1,FETCH_FROM_GLOBAL_CONTIGUOUS,FETCH_FROM_GLOBAL_CONTIGUOUS,0,0));
 }
 
 
